063948cfbb2b50fc97ed59f895f28a359985542f
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-09-29 12:17:12 +0530

    Time: 70 ms (24.56%), Space: 14.1 MB (29.43%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -7,16 +7,12 @@ class Solution:
         
         for i in range((n+1)//2):
             for j in range(n//2):
-                # temp = matrix[n-j-1][i] 
-                # matrix[n-j-1][i] = matrix[n-i-1][n-j-1] 
-                # matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
-                # matrix[j][n-i-1] = matrix[i][j]
-                # matrix[i][j] = temp
-                
                 temp = matrix[i][j] 
                 matrix[i][j] = matrix[n-j-1][i] 
                 matrix[n-j-1][i]  = matrix[n-i-1][n-j-1]
                 matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
                 matrix[j][n-i-1] = temp
+                
+                
                 
                 
