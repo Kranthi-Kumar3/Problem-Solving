dbaf3e01b02f4d8939b7445e85e6bb266e68ee77
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-27 15:16:24 +0530

    Time: 388 ms (25.05%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,10 +1,18 @@
 # Write your MySQL query statement below
 
+# select 
+#     max(salary) as SecondHighestSalary
+# from employee 
+# where salary != (
+#                  select 
+#                      max(salary) 
+#                  from employee
+#                 );
+
+select
+(
 select 
-    max(salary) as SecondHighestSalary
-from employee 
-where salary != (
-                 select 
-                     max(salary) 
-                 from employee
-                );
+    distinct salary 
+from 
+    employee
+order by salary desc limit 1 offset 1) as SecondHighestSalary;
