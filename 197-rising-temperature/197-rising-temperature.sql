60b7a2121d990fd8c08ddec5ec390ce7e80835ae
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-27 16:32:05 +0530

    Time: 829 ms (72.66%), Space: 0B (100.00%) - LeetHub

---
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+select tmp.id
+from 
+    (
+        select
+            id, 
+            recordDate,
+            temperature,
+            lag(recordDate) over(order by recordDate) as prev_day,
+            lag(temperature) over(order by recordDate) as prev_temp
+        from weather
+        order by recordDate
+    ) as tmp
+where 
+    tmp.temperature is not null 
+    and date_sub(tmp.recordDate, interval 1 day) = prev_day
+    and tmp.temperature > tmp.prev_temp;
