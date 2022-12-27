04155d479abc0e38902b70b4d2201c2b93ab6ea0
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-27 16:40:45 +0530

    Time: 1048 ms (28.57%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,18 +1,25 @@
 # Write your MySQL query statement below
 
-select tmp.id
-from 
-    (
-        select
-            id, 
-            recordDate,
-            temperature,
-            lag(recordDate) over(order by recordDate) as prev_day,
-            lag(temperature) over(order by recordDate) as prev_temp
-        from weather
-        order by recordDate
-    ) as tmp
-where 
-    tmp.temperature is not null 
-    and date_sub(tmp.recordDate, interval 1 day) = prev_day
-    and tmp.temperature > tmp.prev_temp;
+# select tmp.id
+# from 
+#     (
+#         select
+#             id, 
+#             recordDate,
+#             temperature,
+#             lag(recordDate) over(order by recordDate) as prev_day,
+#             lag(temperature) over(order by recordDate) as prev_temp
+#         from weather
+#         order by recordDate
+#     ) as tmp
+# where 
+#     tmp.temperature is not null 
+#     and date_sub(tmp.recordDate, interval 1 day) = prev_day
+#     and tmp.temperature > tmp.prev_temp;
+
+
+select today.id
+from
+    weather today
+    inner join weather yesterday on datediff(today.recordDate, yesterday.recordDate) = 1
+where today.temperature > yesterday.temperature;
