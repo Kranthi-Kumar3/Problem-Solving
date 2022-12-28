b69dcfcf662d8ec8f1ffcacbf2abf654f0c1e3d8
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-28 15:38:12 +0530

    Update 1407-top-travellers.sql

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
 
+
+
+The group by should be faster. The row number has to assign a row to all rows in the table. It does this before filtering out the ones it doesn't want.
+
+The second query is, by far, the better construct. In the first, you have to be sure that the columns in the partition clause match the columns that you want. More importantly, "group by" is a well-understood construct in SQL. I would also speculate that the group by might make better use of indexes, but that is speculation.
+
+
 select 
     u.name,
     coalesce(sum(distance), 0) as travelled_distance
