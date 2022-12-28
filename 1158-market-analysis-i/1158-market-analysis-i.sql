3e343171f0d8d5b78113bbeba501ef7bb4456a23
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-28 16:23:51 +0530

    Time: 1454 ms (57.77%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,13 +1,23 @@
 # Write your MySQL query statement below
 
-with orders_2019 as (
-    select 
-        order_id, 
-        year(order_date) as year_order, 
-        buyer_id
-    from Orders
-    where year(order_date) = 2019
-)
+# with orders_2019 as (
+#     select 
+#         order_id, 
+#         year(order_date) as year_order, 
+#         buyer_id
+#     from Orders
+#     where year(order_date) = 2019
+# )
+
+# select
+#     u.user_id as buyer_id,
+#     u.join_date,
+#     ifnull(count(o.order_id), 0) as orders_in_2019
+# from
+#     Users u
+#     left join orders_2019 o on u.user_id = o.buyer_id
+# group by u.user_id ;
+
 
 select
     u.user_id as buyer_id,
@@ -15,5 +25,5 @@ select
     ifnull(count(o.order_id), 0) as orders_in_2019
 from
     Users u
-    left join orders_2019 o on u.user_id = o.buyer_id
+    left join Orders o on u.user_id = o.buyer_id and year(order_date) = 2019
 group by u.user_id ;
