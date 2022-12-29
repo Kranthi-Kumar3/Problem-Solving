4ba786c93570e97a40b73d6cad3dc462422eb35b
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-29 14:17:26 +0530

    Time: 983 ms (15.87%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -5,6 +5,4 @@ select
     e.name as employee
 from
     Employee e
-    inner join Employee m on e.managerId = m.id
-where 
-    e.salary > m.salary ;
+    inner join Employee m on e.managerId = m.id and e.salary > m.salary ;
