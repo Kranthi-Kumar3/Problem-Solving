69e587469aaad370cadc1af44428801157908b18
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2022-12-30 15:45:40 +0530

    Time: 1026 ms (31.54%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,17 +1,32 @@
 # Write your MySQL query statement below
 
-with dept_max_sal as (
-SELECT departmentId, MAX(SALARY) as salary
-from employee
-group by departmentId )
+# with dept_max_sal as (
+# SELECT departmentId, MAX(SALARY) as salary
+# from employee
+# group by departmentId )
+
+# select
+#     d.name as Department,
+#     e.name as Employee,
+#     #max(e.salary) over(partition by e.departmentId order by e.salary desc) as Salary
+#     e.salary as Salary
+# from 
+#     employee e
+#     inner join department d on e.departmentId = d.id
+#     inner join dept_max_sal dms on dms.departmentId = e.departmentId
+# where e.salary = dms.salary
+
 
 select
-    d.name as Department,
-    e.name as Employee,
-    #max(e.salary) over(partition by e.departmentId order by e.salary desc) as Salary
-    e.salary as Salary
+tmp.Department, tmp.Employee, tmp.salary from 
+(
+select
+    d.name as Department, e.name as Employee, e.salary,
+    max(e.salary) over(partition by e.departmentId order by e.salary desc) as Sal
 from 
     employee e
     inner join department d on e.departmentId = d.id
-    inner join dept_max_sal dms on dms.departmentId = e.departmentId
-where e.salary = dms.salary
+    ) tmp
+where tmp.Sal = tmp.salary
+
+
