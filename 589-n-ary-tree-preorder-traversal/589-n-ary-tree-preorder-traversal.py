3cf712d561b4415d60e092b0bdecd139261d7e86
Kranthi Kumar <b.kranthi.kumar333@gmail.com>
2023-01-05 12:53:40 +0530

    Time: 102 ms (41.62%), Space: 16.3 MB (46.37%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.py
@@ -8,16 +8,32 @@ class Node:
 
 
 class Solution:    
+    
+    '''Recursive approach'''
+    
     def preorder(self, root: 'Node') -> List[int]:
-        
         if root is None: return []
-        stack = [root]
         output = []
-        
-        while stack:
-            temp = stack.pop()
-            output.append(temp.val)
-            stack.extend(temp.children[::-1])
-            
+        self.traversal(root, output)
         return output
+    
+    def traversal(self, root, output):
+        # if root is None: return
+        output.append(root.val)
+        for child in root.children:
+            self.traversal(child, output)
+        
+    '''Iterative approach'''
+#         if root is None: return []
+#         stack = [root]
+#         output = []
+        
+#         while stack:
+#             temp = stack.pop()
+#             output.append(temp.val)
+#             stack.extend(temp.children[::-1])
+            
+#         return output
+
+        
         
